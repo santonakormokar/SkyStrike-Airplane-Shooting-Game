d03f9cc3a19eb64abbac1e37d794c76fea3a798b
santonakormokar <santonakormokar.067@gmail.com>
2026-08-17 13:55:01 +0600

    Update ShootCommand.java

--- skystrike/ShootCommand.java
+++ skystrike/ShootCommand.java
@@ -1,13 +1,5 @@
 package skystrike;
 
-/**
- * ShootCommand
- * ------------
- * Tells the active PlayingState to fire the player's current
- * ShootStrategy. Bound to the space bar's key-down. PlayingState's
- * Player.tryShoot() already respects a cooldown, so key-repeat while
- * holding space doesn't cause unlimited fire rate.
- */
 public class ShootCommand implements Command {
 
     private final PlayingState playingState;
